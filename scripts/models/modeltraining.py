8776d386f668d55b8f3a33a614ccb8bdf5de3278
Venla R <venla.e.rantanen@gmail.com>
2025-10-03 14:16:52 +0300

    Add century model (wip)

--- scripts/models/modeltraining.py
+++ scripts/models/modeltraining.py
@@ -10,19 +10,21 @@ from pymongo import MongoClient
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import os
+
 from dotenv import load_dotenv
 load_dotenv()
 
 # To run this model: setup .env with mongo uri and create a virtual environment (preferably outside this git repository), install requirements and then you can run this file in the virtual environment
 
+
 MONGO_URI = os.getenv('MONGO_URI')
 DB_NAME = "Paintings"
 COLLECTION_NAME = "Batch-3"
-N_COLORS = 10
-N_FEATURES = N_COLORS * 4
+N_FEATURES = 40
 RANDOM_STATE = 42
 MAX_ITER = 1000
-DEP_MIN_CLASS_COUNT = 200  # fuck robert
+DEP_MIN_CLASS_COUNT = 200
+CEN_MIN_CLASS_COUNT = 150
 PCA_COMPONENTS = 15
 MIN_NATIONALITY_COUNT = 25
 
@@ -36,10 +38,10 @@ def load_data_from_mongo(uri, db_name, collection_name):
     db = client[db_name]
     collection = db[collection_name]
     projection = {'department': 1,
-                  'palette': 1, 'artistNationality': 1, '_id': 0}
+                  'objectEndDate': 1, 'palette': 1, 'artistNationality': 1, 'objectCentury': 1, '_id': 0}
     documents = list(collection.find(
-        {"palette": {"$exists": True, "$size": N_COLORS},
-         "artistNationality": {"$exists": True, "$ne": ""}},
+        {"palette": {"$exists": True},
+         },
         projection
     ))
     return documents
@@ -56,16 +58,19 @@ def process_documents(documents):
             features.extend([hsv[0], hsv[1], hsv[2], weight / 100.0])
 
         nationality = doc.get('artistNationality', 'Unknown')
-
         if isinstance(nationality, list):
             nationality = nationality[0] if nationality else 'Unknown'
         elif isinstance(nationality, str):
             nationality = nationality.split(';')[0].strip()
+            if nationality == "":
+                nationality = 'Unknown'
 
         data.append({
-            'Department_Str': doc['department'],
-            'Nationality_Str': nationality,
+            'Century': doc['objectCentury'],
+            'Department': doc['department'],
+            'Nationality': nationality,
             'Features': features,
+            'Year': doc['objectEndDate']
         })
 
     df = pd.DataFrame(data)
@@ -105,12 +110,14 @@ def filter_rare_classes(df, column_name, min_count):
 
 
 df_filtered = filter_rare_classes(
-    df_all, 'Department_Str', DEP_MIN_CLASS_COUNT)
+    df_all, 'Department', DEP_MIN_CLASS_COUNT)
+df_filtered = filter_rare_classes(
+    df_filtered, 'Century', CEN_MIN_CLASS_COUNT)
 
 # Final feature matrix and target strings
 X_all = np.array(df_filtered['Features'].tolist())
 print(
-    f"\nFinal dataset size (rares filtered): {X_all.shape[0]} samples.\n")
+    f"\nFinal dataset size (rares filtered): {X_all.shape[0]} samples\n")
 
 # ==============================================================================
 # Feature augmentation: PCA on Color Features and One_Hot Encoding Nationality
@@ -120,8 +127,9 @@ print("\n--- FEATURE AUGMENTATION ---")
 
 
 X_color = np.array(df_filtered['Features'].tolist())
+X_year = df_filtered['Year'].values.reshape(-1, 1)
 
-# PCA on Color Features (increased to 98% variance to retain more info)
+# PCA on Color Features
 scaler_color = StandardScaler()
 X_color_scaled = scaler_color.fit_transform(X_color)
 pca = PCA(n_components=0.98, random_state=RANDOM_STATE)
@@ -130,13 +138,13 @@ print(
     f"\nPCA reduced color features shape: {X_pca.shape}. Components used: {pca.n_components_}")
 
 # One-Hot Encode Nationality Features
-nationality_counts = df_filtered['Nationality_Str'].value_counts()
+nationality_counts = df_filtered['Nationality'].value_counts()
 top_nationalities = nationality_counts[nationality_counts >=
                                        MIN_NATIONALITY_COUNT].index
 
 df_ohe_nationality = pd.get_dummies(
-    df_filtered['Nationality_Str'].where(
-        df_filtered['Nationality_Str'].isin(top_nationalities), 'Other'
+    df_filtered['Nationality'].where(
+        df_filtered['Nationality'].isin(top_nationalities), 'Other'
     ), prefix='Nat', dummy_na=False
 ).astype(int)
 X_nationality = df_ohe_nationality.values
@@ -154,21 +162,35 @@ print(
 
 print("\n--- MODEL TRAINING ---")
 
+le_century = LabelEncoder()
+y_century = le_century.fit_transform(df_filtered['Century'])
+
 le_department = LabelEncoder()
-y_department = le_department.fit_transform(df_filtered['Department_Str'])
+y_department = le_department.fit_transform(df_filtered['Department'])
 
 X = X_all
+X_train_c, X_test_c, y_train_c, y_test_c = train_test_split(
+    X, y_century, test_size=0.2, random_state=RANDOM_STATE, stratify=y_century
+)
 
 X_train_dept, X_test_dept, y_train_dept, y_test_dept = train_test_split(
     X, y_department, test_size=0.2, random_state=RANDOM_STATE, stratify=y_department
 )
 
-print(f"\nTraining set size: {X_train_dept.shape[0]}")
-print(f"Testing set size: {X_test_dept.shape[0]}")
+print(f"\nTraining set size: {X_train_c.shape[0]}")
+print(f"Testing set size: {X_test_c.shape[0]}")
 
 # ==============================================================================
 # Model training
 # ==============================================================================
+
+century_model = RandomForestClassifier(
+    n_estimators=400,
+    max_depth=20,
+    random_state=RANDOM_STATE,
+
+)
+century_model.fit(X_train_c, y_train_c)
 
 department_model = RandomForestClassifier(
     n_estimators=100,
@@ -179,9 +201,23 @@ department_model = RandomForestClassifier(
 department_model.fit(X_train_dept, y_train_dept)
 
 
+full_integer_labels_c = np.arange(len(le_century.classes_))
 full_integer_labels_dept = np.arange(len(le_department.classes_))
 
 print("\n------------------------------")
+
+
+print("\n--- century MODEL EVALUATION ---\n")
+y_pred_c = century_model.predict(X_test_c)
+y_proba_c = century_model.predict_proba(X_test_c)
+decoded_y_test_c = le_century.inverse_transform(y_test_c)
+decoded_y_pred_d = le_century.inverse_transform(y_pred_c)
+
+print("Classification Report:\n", classification_report(
+    decoded_y_test_c, decoded_y_pred_d, zero_division=0))
+print(
+    f"Log Loss (Confidence): {log_loss(y_test_c, y_proba_c, labels=full_integer_labels_c):.4f}")
+print(f"Accuracy: {accuracy_score(y_test_c, y_pred_c):.4f}")
 
 
 print("\n--- DEPARTMENT MODEL EVALUATION ---\n")
@@ -194,3 +230,5 @@ print("Classification Report:\n", classification_report(
 print(
     f"Log Loss (Confidence): {log_loss(y_test_dept, y_proba_dept, labels=full_integer_labels_dept):.4f}")
 print(f"Accuracy: {accuracy_score(y_test_dept, y_pred_dept):.4f}")
+
+# todo: tweak century acc
