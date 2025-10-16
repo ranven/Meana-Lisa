1842aa2276eee30f78a454e71dc41c9aedf81964
f1vme <semmal.business@gmail.com>
2025-10-16 21:14:35 +0300

    ready for deploy

--- meana-lisa/src/app/types.ts
+++ meana-lisa/src/app/types.ts
@@ -5,9 +5,35 @@ type Value = {
 
 type Color = [string, number];
 
+interface SimilarPainting {
+  _id: string;
+  objectID: number;
+  isHighlight: boolean;
+  primaryImage: string;
+  department: string;
+  objectName: string;
+  title: string;
+  artistDisplayName: string;
+  artistNationality: string;
+  artistBeginDate: number;
+  artistEndDate: number;
+  artistWikidata_URL: string;
+  objectBeginDate: number;
+  objectEndDate: number;
+  medium: string;
+  dimensions: string;
+  classification: string;
+  objectURL: string;
+}
+
+interface SimilarPalette {
+  similar: SimilarPainting[];
+}
+
 interface Meana {
   department: Value;
   nat: Value;
   century: Value;
   palette: Color[];
+  similar_palette: SimilarPalette;
 }
